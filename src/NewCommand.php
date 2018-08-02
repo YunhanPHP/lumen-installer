e8ea12918b0d6e27ab774754af0c0832449a6124
raygu0101 <raygu0101@gmail.com>
2018-08-02 15:54:19 +0800

    [*]默认安装dev-master，保证是最新版本

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -53,7 +53,7 @@ class NewCommand extends \Lumen\Installer\Console\NewCommand
             $composer . ' config repositories.lumen-app-installer git https://github.com/YunhanPHP/lumen-require-dev.git',
             $composer . ' config repositories.lumen-dev-db-doc git https://github.com/YunhanTech/laravel-db-doc.git',
             $composer . ' config repositories.lumen-dev-yaml-swagger git https://github.com/YunhanTech/swagger-lumen-yaml.git',
-            $composer . ' require --dev yunhanphp/lumen-require-dev ^1.0',
+            $composer . ' require --dev yunhanphp/lumen-require-dev dev-master',
 
             // 复制 env 到本地
             $composer . ' run-script post-root-package-install'
